3108c159e9b9b30726e1365b15e26c3e79260e7b
DanielJaraba1 <130751273+DanielJaraba1@users.noreply.github.com>
2024-02-09 13:08:03 +0100

    Update Usuario.php

--- JARABA/entity/Usuario.php
+++ JARABA/entity/Usuario.php
@@ -1,160 +1,192 @@
 <?php
 
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
 
 #[ORM\Entity] 
 #[ORM\Table(name: 'usuario')]
 class Usuario implements UserInterface, PasswordAuthenticatedUserInterface
 {
-	#[ORM\Id]
-    #[ORM\Column(type:'integer', name:'IdUsuario')]
-    #[ORM\GeneratedValue]
-    private $id;
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer', name: 'IdUsuario')]
+    private $id_usuario;
 
-	#[ORM\Column(type:'string', name:'NombreUsuario')]
+    #[ORM\Column(type:'string', name:'NombreUsuario')]
     private $nombre_usuario;
 
-	#[ORM\Column(type:'string', name:'CorreoUsuario')]
+    #[ORM\Column(type:'string', name:'CorreoUsuario')]
     private $correo_usuario;
 
-	#[ORM\Column(type:'string', name:'ContraseñaUsuario')]
+    #[ORM\Column(type: 'string', name: 'ContrasenaUsuario')]
     private $contraseña_usuario;
 
-	#[ORM\Column(type:'string', name:'Descripcion')]
+    #[ORM\Column(type:'string', name:'Descripcion')]
     private $descripcion;
 
-	#[ORM\Column(type:'string', name:'Localidad')]
+    #[ORM\Column(type:'string', name:'Localidad')]
     private $localidad;
 
-	#[ORM\Column(type:'date', name:'FechaDeNacimiento')]
-    private $fechanacimiento;
+    #[ORM\Column(type: 'date', name: 'FechaNacimiento')]
+    private $fecha_nacimiento;
 
-	#[ORM\Column(type:'boolean', name:'UsuarioAdmin')]
+    #[ORM\Column(type:'boolean', name:'UsuarioAdmin')]
     private $admin;
 
     #[ORM\OneToMany(targetEntity:'Comentario', mappedBy:'usuario')]
     private $comentarios;
 
     #[ORM\OneToMany(targetEntity:'PostUsuario', mappedBy:'usuario')]
-    private $posts_usuario;
+    private $postsUsuario;
 
-    #[ORM\OneToMany(targetEntity:'Amistad', mappedBy:'usuario')]
+    #[ORM\OneToMany(targetEntity: "App\Entity\Amistad", mappedBy: "usuario")]
     private $amistades;
 
-    public function getUsuarioAdmin(){
+    public function __construct()
+    {
+       $this->amistades = new ArrayCollection();
+    }
+
+    public function getUsuarioAdmin(): bool
+    {
        return $this->admin;
     }
 
-    public function setUsuarioAdmin($admin){
+    public function setUsuarioAdmin(bool $admin): void
+    {
         $this->admin = $admin;
     }
 
-    public function getIdUsuario(){
-        return $this->id;
+    public function getIdUsuario(): ?int
+    {
+        return $this->id_usuario;
     }
-    public function setIdUsuario($id){
-        return  $this->id = $id;
+
+    public function setIdUsuario(int $id_usuario): void
+    {
+        $this->id_usuario = $id_usuario;
     }
-    public function setNombreUsuario($nombre_usuario){
-        $this->nombre_usuario = $nombre_usuario;
-    }
-    public function getNombreUsuario(){
+
+    public function getNombreUsuario(): ?string
+    {
         return $this->nombre_usuario;
     }
-    public function setCorreoUsuario($correo_usuario){
-        $this->correo_usuario = $correo_usuario;
+
+    public function setNombreUsuario(string $nombre_usuario): void
+    {
+        $this->nombre_usuario = $nombre_usuario;
     }
-    public function getCorreoUsuario(){
+
+    public function getCorreoUsuario(): ?string
+    {
         return $this->correo_usuario;
     }
-    public function setContraseñaUsuario($contraseña_usuario){
-        $this->contraseña_usuario = $contraseña_usuario;
+
+    public function setCorreoUsuario(string $correo_usuario): void
+    {
+        $this->correo_usuario = $correo_usuario;
     }
-	public function getContraseñaUsuario(){
+
+    public function getContraseñaUsuario(): ?string
+    {
         return $this->contraseña_usuario;
     }
 
-    public function setDescripcion($descripcion) {
-        $this->descripcion = $descripcion;
+    public function setContraseñaUsuario(string $contraseña_usuario): void
+    {
+        $this->contraseña_usuario = $contraseña_usuario;
     }
 
-    public function getDescripcion(){
+    public function getDescripcion(): ?string
+    {
         return $this->descripcion;
     }
 
-    public function setLocalidad($localidad){
-        $this->localidad = $localidad;
+    public function setDescripcion(string $descripcion): void
+    {
+        $this->descripcion = $descripcion;
     }
 
-    public function getLocalidad(){
+    public function getLocalidad(): ?string
+    {
         return $this->localidad;
     }
 
-    public function setFechaDeNacimiento($fechanacimiento){
-        $this->fechanacimiento = $fechanacimiento;
+    public function setLocalidad(string $localidad): void
+    {
+        $this->localidad = $localidad;
     }
 
-    public function getFechaDeNacimiento(){
-        return $this->fechanacimiento;
+    public function getFechaNacimiento(): ?\DateTimeInterface
+    {
+        return $this->fecha_nacimiento;
     }
 
-    public function setComentario($comentarios){
-        $this->comentarios = $comentarios;
+    public function setFechaNacimiento(\DateTimeInterface $fecha_nacimiento): void
+    {
+        $this->fecha_nacimiento = $fecha_nacimiento;
     }
 
-    public function getComentario(){
+    public function getComentarios(): ArrayCollection
+    {
         return $this->comentarios;
     }
 
-    public function setPostUsuario($posts_usuario){
-        $this->posts_usuario = $posts_usuario;
+    public function setComentarios(ArrayCollection $comentarios): void
+    {
+        $this->comentarios = $comentarios;
     }
 
-    public function getPostUsuario(){
-        return $this->posts_usuario;
+    public function getPostsUsuario(): ArrayCollection
+    {
+        return $this->postsUsuario;
     }
 
-    public function setAmistad($amistades){
-        $this->amistades = $amistades;
+    public function setPostsUsuario(ArrayCollection $postsUsuario): void
+    {
+        $this->postsUsuario = $postsUsuario;
     }
 
-    public function getAmistad(){
+    public function getAmistades(): ArrayCollection
+    {
         return $this->amistades;
     }
 
-
-
-	public function getRoles(): array
+    public function setAmistades(ArrayCollection $amistades): void
     {
-        if($this->admin == 1){
-            return ['ROLE_USER','ROLE_ADMIN'];   
-        }else{
-            return ['ROLE_USER'];    
+        $this->amistades = $amistades;
+    }
+
+    public function getRoles(): array
+    {
+        if ($this->admin == 1) {
+            return ['ROLE_USER', 'ROLE_ADMIN'];
+        } else {
+            return ['ROLE_USER'];
         }
-		        
-	}
+    }
 
     public function getPassword(): string
     {
-        return $this->getContraseñaUsuario();
+        return $this->contraseña_usuario;
     }
-
 
     public function getUserIdentifier(): string
     {
-        return $this->getCorreoUsuario();
+        return $this->nombre_usuario;
     }
 
     public function getSalt(): ?string
     {
         return null;
     }
-	
+
     public function eraseCredentials(): void
     {
-
+        // No es necesario hacer nada aquí, pero el método debe ser implementado
     }
 }
