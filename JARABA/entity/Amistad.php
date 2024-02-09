f48213193dd168c69558b79dc1c1600575c8cae3
DanielJaraba1 <130751273+DanielJaraba1@users.noreply.github.com>
2024-02-09 13:08:54 +0100

    Update Amistad.php

--- JARABA/entity/Amistad.php
+++ JARABA/entity/Amistad.php
@@ -1,52 +1,68 @@
 <?php
 
 namespace App\Entity;
+
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity] 
+#[ORM\Entity]
 #[ORM\Table(name: 'amistad')]
-class Amistad 
+class Amistad
 {
-	#[ORM\Id]
-    #[ORM\Column(type:'integer', name:'IdUsusario1')]
-    #[ORM\GeneratedValue]
-    private $id_usuario_1;
 
-	#[ORM\Column(type:'integer', name:'IdUsusario1')]
-    private $id_usuario_2;
+    #[ORM\Id]
+    #[ORM\GeneratedValue(strategy: 'AUTO')]
+    #[ORM\Column(type: 'integer', name: 'IdAmistad')]
+    private $id_amistad;
 
-	#[ORM\Column(type:'integer', name:'Aceptado')]
-    private $aceptado;
+    #[ORM\ManyToOne(targetEntity: 'App\Entity\Usuario')]
+#[ORM\JoinColumn(name: 'IdUsuario1', referencedColumnName: 'IdUsuario')]
+private $usuario1;
 
-    #[ORM\ManyToOne(targetEntity:'Usuario', inversedBy:'amistad')]
-	#[ORM\JoinColumn(name:'Usuario', referencedColumnName:'IdUsuario')]
-    private $usuario; 
+#[ORM\ManyToOne(targetEntity: 'App\Entity\Usuario')]
+#[ORM\JoinColumn(name: 'IdUsuario2', referencedColumnName: 'IdUsuario')]
+private $usuario2;
 
-    public function getIdUsusario1(){
-       return $this->id_usuario_1;
+    #[ORM\Column(type: 'string', name: 'Estado')]
+    private $estado;
+
+    public function getId()
+    {
+        return $this->id_amistad;
+    }
+    
+
+    public function setIdAmistad($id_amistad)
+    {
+        $this->id_amistad = $id_amistad;
     }
 
-    public function setIdUsusario1($id_usuario_1){
-        $this->id_usuario_1 = $id_usuario_1;
+    public function getUsuario1()
+    {
+        return $this->usuario1;
     }
 
-    public function getIdUsusario2(){
-        return $this->id_usuario_2;
-     }
- 
-     public function setIdUsusario2($id_usuario_2){
-         $this->id_usuario_2 = $id_usuario_2;
-     }
-    
-     public function getAceptado(){
-        return $this->aceptado;
-     }
- 
-     public function setAceptado($aceptado){
-         $this->aceptado = $aceptado;
-     }
-    
-     public function getUsuario(){
-        return $this->usuario;
-     }
+    public function setUsuario1($usuario1)
+    {
+        $this->usuario1 = $usuario1;
+    }
+
+    public function getUsuario2()
+    {
+        return $this->usuario2;
+    }
+
+    public function setUsuario2($usuario2)
+    {
+        $this->usuario2 = $usuario2;
+    }
+
+    public function getEstado()
+    {
+        return $this->estado;
+    }
+
+    public function setEstado($estado)
+    {
+        $this->estado = $estado;
+    }
 }
